cab95bbfeb4663b2148b8b4c455caf66bf18253c
Daria <muzzy404@gmail.com>
2017-05-24 19:46:30 +0300

    creating process

--- app/src/main/java/daria/sem4/labworks/menuapp/POJOs/Table.java
+++ app/src/main/java/daria/sem4/labworks/menuapp/POJOs/Table.java
@@ -10,16 +10,34 @@ public class Table {
 
     private final int id;
     private final Waiter waiter;
+    private int personsNum;
     private ArrayList<Order> orders;
 
-    public Table(int id, Waiter waiter) {
+    public Table(int id, Waiter waiter, int personsNum) {
         this.id = id;
         this.waiter = waiter;
+        this.personsNum = personsNum;
         orders = new ArrayList<>();
+    }
+
+    void addPerson() {
+        ++personsNum;
+    }
+
+    void removePerson() {
+        --personsNum;
+    }
+
+    int getPersonsNum() {
+        return personsNum;
     }
 
     void addOrder(Order order) {
         orders.add(order);
+    }
+
+    int ordersNum() {
+        return orders.size();
     }
 
 }
